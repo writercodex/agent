44c55cf22a31d14916415e9bfbbd327a70ead500
writercodex <leonguin3@gmail.com>
2026-06-02 04:02:12 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,13 @@
 print("STEP 1")
 
-from memory import init_db
+from memory import init_db, save_memory
 
 print("STEP 2")
 
 init_db()
 
 print("STEP 3")
+
+save_memory("owner_name", "Jhon")
+
+print("STEP 4")
