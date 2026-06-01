e5046d68a42f84148b6da28f19242de836a361d1
writercodex <leonguin3@gmail.com>
2026-06-02 05:40:28 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,55 @@
+from telegram import Update
+from telegram.ext import (
+    Application,
+    MessageHandler,
+    CommandHandler,
+    ContextTypes,
+    filters
+)
+
+from config import TELEGRAM_TOKEN, OWNER_ID
 from ai import chat_with_ai
 
-print("STEP 1")
 
-try:
+async def ping(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE
+):
+    if update.effective_user.id != OWNER_ID:
+        return
 
-    print("STEP 2")
+    await update.message.reply_text("pong")
 
-    reply = chat_with_ai("Halo")
 
-    print("STEP 3")
+async def chat(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE
+):
+    if update.effective_user.id != OWNER_ID:
+        return
 
-    print(reply)
+    user_message = update.message.text
 
-except Exception as e:
+    reply = chat_with_ai(user_message)
 
-    print("ERROR:")
+    await update.message.reply_text(reply)
 
-    print(str(e))
+
+app = Application.builder().token(
+    TELEGRAM_TOKEN
+).build()
+
+app.add_handler(
+    CommandHandler("ping", ping)
+)
+
+app.add_handler(
+    MessageHandler(
+        filters.TEXT & ~filters.COMMAND,
+        chat
+    )
+)
+
+print("Telegram bot running...")
+
+app.run_polling()
