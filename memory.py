9554702c3b46995b341d1289f646df9dedfc8415
writercodex <leonguin3@gmail.com>
2026-06-02 03:26:50 +0700

    Update memory.py

--- memory.py
+++ memory.py
@@ -17,3 +17,41 @@ def init_db():
 
     cur.close()
     conn.close()
+
+
+def save_memory(key, value):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute("""
+    INSERT INTO memories(memory_key, memory_value)
+    VALUES (%s, %s)
+    ON CONFLICT (memory_key)
+    DO UPDATE SET
+        memory_value = EXCLUDED.memory_value
+    """, (key, value))
+
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+
+def get_memory(key):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute(
+        "SELECT memory_value FROM memories WHERE memory_key=%s",
+        (key,)
+    )
+
+    row = cur.fetchone()
+
+    cur.close()
+    conn.close()
+
+    if row:
+        return row[0]
+
+    return None
