0fe6945f158253a0b2392eccdcbb52deac3f4b9a
writercodex <leonguin3@gmail.com>
2026-06-02 03:34:23 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+import time
+
 from memory import init_db, save_memory, get_memory
 
 print("Initializing memory system...")
@@ -9,3 +11,5 @@ save_memory("owner_name", "Jhon")
 name = get_memory("owner_name")
 
 print(f"Memory result: {name}")
+
+time.sleep(60)
