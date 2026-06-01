8d33fd734260ecc14c90cf9ad7c257ec459234d2
writercodex <leonguin3@gmail.com>
2026-06-02 03:27:18 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,11 @@
-from memory import init_db
+from memory import init_db, save_memory, get_memory
 
-print("Creating memory table...")
+print("Initializing memory system...")
 
 init_db()
 
-print("Memory table ready!")
+save_memory("owner_name", "Jhon")
+
+name = get_memory("owner_name")
+
+print(f"Memory result: {name}")
